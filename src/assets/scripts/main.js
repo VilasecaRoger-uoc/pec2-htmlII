79c3875d6476e0931c1770afc58fe5d4fb2a2b72
unknown <rvilasecar@uoc.edu>
2021-11-30 21:41:55 +0100

    First commit

---
+++ src/assets/scripts/main.js
@@ -0,0 +1,16 @@
+/**
+ * Import dependencies from node_modules
+ * see commented examples below
+ */
+ import 'font-awesome/scss/font-awesome.scss';
+// import 'some-node-module';
+// import SomeModule from 'some-node-module';
+
+/**
+ * Write any other JavaScript below
+ */
+
++( function() {
+  const university = "UOC";
+  console.log(`Hello, ${university}!`);
+} )();
